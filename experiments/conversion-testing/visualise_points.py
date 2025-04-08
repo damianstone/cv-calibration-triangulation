fa9af58476e876fb7bab1da7e7d3585286d6a0fa
1uked <123551653+1uked@users.noreply.github.com>
2025-04-08 15:33:27 +0100

    shift for shot 1661

--- experiments/conversion-testing/visualise_points.py
+++ experiments/conversion-testing/visualise_points.py
@@ -7,19 +7,28 @@ from matplotlib.widgets import Slider
 # ---------------------
 # Load CSV and Process
 # ---------------------
-df = pd.read_csv("data/stereo_detections_triangulated.csv").head(16) #.head(27).tail(10)
+df = pd.read_csv("data/stereo_detections_triangulated.csv").head(25)
+
+# Ensure 'anomaly_detected' is treated as boolean
+# df['anomaly_detected'] = df['anomaly_detected'].astype(str) == 'True'
+
+# original_count = len(df)
+# df = df[df['anomaly_detected'] == False].copy()
+# removed_count = original_count - len(df)
+
+# print(f"Removed {removed_count} anomalous points")
 
 # frame 973 is bounce
 # 1011 is return
 
-offset_to_origin = np.array([-5.7, 0.2, 1.7]) # Apply after converting pixels to metres
-# offset_to_origin = np.array([0, 0, 0])
+# offset_to_origin = np.array([-5.7, -3.0, 1.7]) # Apply after converting pixels to metres
+offset_to_origin = np.array([10.4, -6.8, 1.7])
 
 # Parse the 3D position strings and convert from mm to meters
 trajectory = df['position_3d_pixels'].apply(lambda s: np.array(eval(s)) / 1000.0)
 trajectory = np.vstack(trajectory.values)
 
-x = trajectory[:, 2] + offset_to_origin[0]  # Horizontal position (court width)
+x = -trajectory[:, 2] + offset_to_origin[0]  # Horizontal position (court width)
 y = trajectory[:, 0] + offset_to_origin[1]  # Court length
 z = -(trajectory[:, 1]) + offset_to_origin[2]  # Height above court
 
@@ -92,11 +101,13 @@ ax.set_zlabel("Height (m)")
 ax.legend()
 
 slider_ax = fig.add_axes([0.35, 0.05, 0.3, 0.05])
-slider = Slider(ax=slider_ax, label='', color='#aabfaa',
+slider = Slider(ax=slider_ax, label='Frame', color='#aabfaa',
                 valmin=1, valmax=len(x), valinit=len(x), valstep=1)
-slider.valtext.set_visible(False)
 slider.poly.set_edgecolor("black")
 slider.poly.set_linewidth(2)
+
+# Display frame number on the right
+slider.valtext.set_text(f"{len(x)}/{len(x)}")  # Initialize text
 
 def update(val):
     idx = int(slider.val)
@@ -107,6 +118,7 @@ def update(val):
     line.set_data(x[:idx], y[:idx])
     line.set_3d_properties(z[:idx])
     scatter_container[0] = new_scatter
+    slider.valtext.set_text(f"{idx}/{len(x)}")  # Update frame count
     fig.canvas.draw_idle()
 
 slider.on_changed(update)
